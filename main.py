7fa1690aa7e4066bcd22d0d4853276d8caf49b2c
Daviiihr <david.fernandez2024@alu.uct.cl>
2026-09-15 18:55:01 -0300

    The main.py file was completed

--- main.py
+++ main.py
@@ -1,32 +1,25 @@
-# Funcion de analisis
+from scanner import analyze_email
 
-def analyze_phishing(affair, body):
-    full_text = f"{affair} {body}"
-    risk_score = 0
-    warning = []
+if __name__ == "__main__":
+    test_subject ="=?utf-8?q?Acci=C3=B3n_Requerida=3A_Cuenta_Bloqueada?="
 
-    pattern_search = pattern.findall(full_text)
-    if pattern_search:
-        risk_score += len(set(pattern_search)) * 2
-        warning.append(f"Keywords found: {set(pattern_search)}")
+    body_test = """
+        <html>
+          <body>
+            <p>Estimado usuario, se requiere acci&#243;n <b>urgente</b>.</p>
+            <p>Su c\u200buen\u200bta ha sido suspendida.</p>
+            <div style="display:none">Este texto es ignorado por el usuario pero confunde a los filtros</div>
+            <p>Verifique su identidad aqu&#x2F;:
+               <a href="http://xn--googl-0qa.com/login">https://www.google.com/login</a>
+            </p>
+          </body>
+        </html>
+        """
+    result = analyze_email(test_subject, body_test)
 
-    urls = url_pattern.findall(full_text)
-    for url in urls:
-        if IP_URL_PATTERN.match(url):
-            risk_score += 5
-            warning.append(f"IP URL found: {url}")
-
-        if any(shorts in url for shorts in SHORTS):
-            risk_score += 3
-            warning.append(f"Short URL found: {url}")
-
-        if any(domain in url for domain in SUSPICIOUS_DOMAIN):
-            risk_score += 4
-            warning.append(f"Suspicious domain found: {url}")
-
-    resultado = {
-        "Risk Score": risk_score,
-        "Warning": warning,
-        "Is phishing": risk_score >=5
-    }
-    return resultado
+    print("=== Analysis Report ===")
+    print(f"Score: {result['Risky score']}")
+    print(f"Veredict: {'PHISHING DETECTED' if result['Is phishing'] else 'Clean Email'}")
+    print("Warning    :")
+    for warning in result['warning']:
+        print(f" [!] {warning}")
